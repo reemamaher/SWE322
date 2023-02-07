36915c3883fd20caf5e4adea5bf689aa1f7498e8
reemamaher <al7arbireema@gmail.com>
2023-02-07 15:12:30 +0300

    Lab work

---
+++ SWE322/Lab_Work/Lab2/Reem_Lab2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body> 
+<?php
+
+
+function add ($x, $y){ 
+return ($x + $y);
+}
+
+echo add (3,4);
+
+Echo "<br/>";
+
+function subtract ($x, $y){ 
+return ($x - $y);
+}
+
+echo subtract (10,5);
+
+echo "<br/>";
+
+function multiply ($x, $y){ 
+return ($x * $y);
+}
+
+echo multiply (10,5);
+
+echo "<br/>";
+
+function divide ($x, $y){ 
+return ($x / $y);
+}
+
+echo divide (10,5);
+
+
+
+     ?>
+</body>
+</html>
---
+++ SWE322/Lab_Work/Lab2/Reem_lab1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body> 
+
+<?php
+$n=1;
+
+echo $n=1;
+
+echo "<br/>";
+
+$s="1";
+
+echo $s;
+
+echo "<br/>";
+
+$n1= $n . $s ;
+
+echo $n1;
+
+echo "<br/>";
+
+$n2 = $n1 . "10 little penguins";
+
+echo $n2;
+
+echo "<br/>";
+
+$n3 = "Hello";
+
+Echo $n3;
+
+echo "<br/>";
+
+$n4 = "World";
+
+Echo $n4;
+
+echo "<br/>";
+
+$n5 = $n3 . $n4;
+
+echo $n5;
+
+echo "<br/>";
+
+$n6 = $n3 . $n4;
+
+echo $n6;
+
+
+
+
+
+
+
+     ?>
+</body>
+</html>
